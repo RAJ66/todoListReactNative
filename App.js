f5fd819cf5d437960ab85fda83e3af6aeac4d58f
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-03 16:25:46 +0100

    Style List

--- App.js
+++ App.js
@@ -6,24 +6,24 @@
  * @flow
  */
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
+  Button,
   StyleSheet,
-  ScrollView,
+  TextInput,
   View,
   Text,
   StatusBar,
-  FlatList,
-} from "react-native";
+  FlatList
+} from 'react-native';
 
 import {
   Header,
   LearnMoreLinks,
   Colors,
   DebugInstructions,
-  ReloadInstructions
-} from "react-native/Libraries/NewAppScreen";
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
 //const App = () => {
 type Props = {};
@@ -31,17 +31,19 @@ class App extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      text: "",
       items: [
-        { key: "0", desc: "Item 1", done: false },
-        { key: "1", desc: "Item 2", done: false }
-      ],
+        { key: '0', desc: 'Item 1', done: false },
+        { key: '1', desc: 'Item 2', done: false },
+      ]
     };
   }
 
   renderItem(obj) {
-    return <Text>{obj.item.desc}</Text>;
+    return <Text style={styles.cell}>{obj.item.desc}</Text>;
   }
+
+  insertItem() {}
 
   render() {
     return (
@@ -51,6 +53,20 @@ class App extends Component<Props> {
           data={this.state.items}
           renderItem={this.renderItem}
         />
+        <View style={styles.intupView}>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => {
+              this.setState({ text });
+            }}
+            value={this.state.text}
+          />
+          <Button
+            style={styles.Button}
+            onPress={this.insertItem}
+            title="Inserir"
+          />
+        </View>
       </View>
     );
   }
@@ -59,13 +75,35 @@ class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+
+    backgroundColor: '#F5FCFF',
   },
   list: {
-    marginTop: 24,
-  }
+    marginTop: 24
+  },
+  cell: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 20,
+    backgroundColor: "#E4EBEE",
+    fontSize: 18,
+    marginBottom: 2
+  },
+  input: {
+    backgroundColor: 'white',
+    borderColor: '#CCC',
+    borderWidth: 3,
+    padding: 10,
+    margin: 20,
+    flex: 1
+  },
+  intupView: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: 10
+  },
 });
 
 export default App;
